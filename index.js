5a8f75a5c8e75bb326e32971c79e08960eac651a
Itay <weinbergitay@gmail.com>
2022-04-29 01:47:24 +0300

    arrange functions, 2 players game, set timeout

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-
 function shuffle(arr){
-    let newArr = arr, shuffledArr = [], card1 = "", card2 = "", i =0, i2 = 0;
+    let  newArr = arr, shuffledArr = [], card1 = "", card2 = "", i =0, i2 = 0;
         newArr = newArr.concat(arr);
     shuffledArr = newArr;
     for(n of newArr){
@@ -12,66 +11,105 @@ function shuffle(arr){
 }
 return shuffledArr;
 }
-let cards = ["😜", "🥰", "🍕", "🐨", "💐"];
- arr = shuffle(cards), counter = 0, counterAr = [],
- pairsCounter = 0, openedCards = [];
- board = document.getElementById("game-table");
-for(i in arr){
+function openCards(arr,elem){
+    if(!(arr.find((value)=> value == elem))){
+        arr.push(elem);
+        }
+        elem.innerText = shuffledCards[elem.id];
+        return arr;
+}
+let timeout;
+function isEqual(arr){
+        if(arr[0].innerText == arr[1].innerText){
+                timeout = setTimeout(equalCards,2000,arr);
+                alert("well done!");
+                pairsCounter++;
+                if(pairsCounter == cards.length){
+                    alert((players[0].score > players[1].score)? `${players[0].playerName} wins`:
+                    `${players[1].playerName} wins`);
+                }
+            return true;
+    }
+        else{  
+           timeout = setTimeout(closeCards,2000,arr);
+            turn++;
+            return false;
+        }
+    }
+function closeCards(arr){
+    arr[0].innerText = "";
+    arr[1].innerText = "";
+    return arr;
+}
+function equalCards(arr){
+    arr[0].className = "equal";
+    arr[1].className = "equal";
+    return arr;
+}
+function changePlayer(obj){
+    board.id = obj.playerName;
+    document.getElementById("headline").innerText = `Memory Game: ${obj.playerName}`;
+    document.getElementById("score").innerText = `Score: ${obj.score}`;    
+}
+    
+    
+let cards = ["😜", "🥰", "🍕", "🐨", "💐"],
+ shuffledCards = shuffle(cards), counter = 0, pairsCounter = 0, counterAr = [], 
+ openedCards = [], turn = 0,
+ players = [
+     {playerName: "Player1", score: 0},
+     {playerName: "Player2", score: 0},
+ ];
+ const board = document.getElementById("Player1");
+for(i in shuffledCards){
     let elem = document.createElement("div");
-    elem.className = "card"
+    elem.className = "card";
     elem.id = i;
-    elem.onmouseover = function(){
-        elem.className = "card-over"
-}
-    elem.onmouseleave = function(){
-        elem.className = "card"
-}
     board.appendChild(elem);
 }
-for(i in arr){
+for(i in shuffledCards){
     counterAr[i] = 0;
     document.getElementById(i).onclick = function(){
     counterAr[this.id]++;
     let dc = counterAr.find((value)=>value > 1);
     if(counter < 2 || (dc && counter < dc + 1)){
         counter++;
-        if(!(openedCards.find((value)=> value == this))){
-        openedCards.push(this);
-        }
-        this.innerText = arr[this.id];
-            if( openedCards.length > 1 && openedCards[0].innerText == openedCards[1].innerText){
-                openedCards[0].className = "equal";
-                openedCards[1].addEventListener("mouseleave", ()=> {
-                    this.className = "equal"
-                })
-                alert("well done!")
-                counter = 0;
-                pairsCounter++;
-                if(pairsCounter == cards.length){ 
-                    alert("congrats! you won the game!");
+        if(turn % 2 == 0){
+            openedCards = openCards(openedCards,this);
+            if(openedCards.length > 1){
+                if(isEqual(openedCards)){
+                    players[0].score++;
+                    document.getElementById("score").innerText = `Score: ${players[0].score}`;    
+                } 
+                else{
+                    timeout = setTimeout(changePlayer,2500,players[1]);
                 }
+                counter = 0;
                 openedCards = [];
                 for(i in counterAr){
                     counterAr[i] = 0;
-            }
-    }
-}
-        else{  
-            openedCards[0].innerText = "";
-            openedCards[1].innerText = "";
-            counter = 0;
-            openedCards = [];
-            for(i in counterAr){
-                counterAr[i] = 0;
+            }     
+        }
+        }
+        else{
+            openedCards = openCards(openedCards,this);
+            if(openedCards.length > 1){
+                if(isEqual(openedCards)){
+                    players[1].score++;
+                    document.getElementById("score").innerText = `Score: ${players[1].score}`;    
+
+                }
+                else{
+                    timeout = setTimeout(changePlayer,2500,players[0]);
+                }
+                counter = 0;
+                    openedCards = [];
+                    for(i in counterAr){
+                        counterAr[i] = 0;
+                }  
             }
         }
-    } 
+    }
+}      
 }
-
-
-
-
-
-
-   
 
